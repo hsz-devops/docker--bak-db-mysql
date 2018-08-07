bce3b930ebc78aedda36397172686a7bf795827f
Joao C Costa <joaocc-dev@live.com>
2018-08-07 11:50:20 +0100

    mysql can now receive parameters

--- bak-run-5.7/src/entrypoint.sh
+++ bak-run-5.7/src/entrypoint.sh
@@ -5,4 +5,4 @@ set -x
 # Blocking all other entrypoint from base images, since the only reason
 # we are using base MySQL Server image is to have correct mysqldump binaries
 
-exec "$@"
+exec mysqldump "$@"
